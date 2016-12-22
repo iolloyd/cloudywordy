a3441aa515c91d0841600f2ac09a38973f41f6cb
Lloyd Moore <manchesterboy@gmail.com>
2016-12-22 17:58:19 +0000

    cleans up some print statements

--- handlers/main_handler.py
+++ handlers/main_handler.py
@@ -9,7 +9,5 @@ class MainHandler(RequestHandler):
 
     def post(self):
         url = self.get_argument('url')
-        print('saving')
         save_top_words(url)
-        print('saved')
         self.redirect('/')
