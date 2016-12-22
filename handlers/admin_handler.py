f147be1229be7d461c92fe8239a39f27d9bfc605
Lloyd Moore <manchesterboy@gmail.com>
2016-12-22 19:02:17 +0000

    make the output sorted in reverse

--- handlers/admin_handler.py
+++ handlers/admin_handler.py
@@ -3,9 +3,12 @@ from lib import get_data_from_cloudsql
 from lib.cryption import decrypt_word
 
 
+def get_key(item):
+    return item[1]
+
 class AdminHandler(RequestHandler):
     def get(self):
         counts = get_data_from_cloudsql()
         counts = [(decrypt_word(x[0]), x[1]) for x in counts]
-        print(counts)
+        counts = sorted(counts, key=get_key, reverse=True)
         self.render('../templates/admin.html', counts=counts)
